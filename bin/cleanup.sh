3136a225045f184ee469c3b01555ee40cab6d037
Gez Page <gezpage@gmail.com>
2013-06-23 11:08:44 +0100

    Created dotfiles script to condense sprawling tasks

---
+++ bin/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bin_path=/home/gez/bin
+
+rm -v ${bin_path}/vim_update.sh
+rm -v ${bin_path}/vim_update_quick.sh
+rm -v ${bin_path}/install-dotfiles.sh
+rm -v ${bin_path}/install-vim.sh
