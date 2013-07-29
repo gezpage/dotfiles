31cdb1819914f05f6fc6b09e6779d565b76e8cac
Gez Page <gezpage@gmail.com>
2013-07-29 20:23:14 +0100

    typo

--- bin/dotfiles-post-install.sh
+++ bin/dotfiles-post-install.sh
@@ -3,4 +3,4 @@
 source=~/.dotfiles/
 
 rm ~/.oh-my-zsh/themes/powerline.zsh-theme
-ln -s ${source}moddules/oh-my-zsh-powerline-theme/powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
+ln -s ${source}modules/oh-my-zsh-powerline-theme/powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
