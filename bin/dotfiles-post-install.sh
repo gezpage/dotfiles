f008a84d877d269a117b3e8bcb94ab3b1b439d96
Gez Page <gezpage@gmail.com>
2013-07-30 18:52:22 +0100

    Supress errors in post install script

--- bin/dotfiles-post-install.sh
+++ bin/dotfiles-post-install.sh
@@ -2,5 +2,5 @@
 
 source=~/.dotfiles/
 
-rm ~/.oh-my-zsh/themes/powerline.zsh-theme
+rm ~/.oh-my-zsh/themes/powerline.zsh-theme >/dev/null 2>&1
 ln -s ${source}modules/oh-my-zsh-powerline-theme/powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
